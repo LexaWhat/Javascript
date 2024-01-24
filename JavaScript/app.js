0a7a42f72e0cb2b6d7e36c4b0ef63f6b56fbf8f2
LexaWhat <udiejfkskfid@gmail.com>
2024-01-24 19:12:48 +0500

    Lesson 5

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,20 @@
-let a = 1;
+//base arithmetic operators
 
-for(i=1; i<10; i++) {
-    console.log(i)
-}
+const width = 10;
+const height = 5;
+const space = 10 * 5;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 * 2 * 2;
+const volume2 = 2 ** 3;
+console.log(space);
+console.log(division);
+console.log(volume);
+console.log(volume2);
 
-console.log(a)
-a = "test";
-console.log(a)
+//strings
+const city = "Moscow";
+const street = "Novoslobodskaya";
+
+console.log(city,street,"5")
