c7d61a3339539056871f22eaac3acf4ea546a640
LexaWhat <udiejfkskfid@gmail.com>
2024-01-25 20:47:32 +0500

    lesson 10

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,15 +1,5 @@
-const age = "18";
-console.log(Number(age) + 5);
-console.log(age - 3);
-const userName = "Vasya";
-console.log(Number(userName) + 5);
-console.log(String(1) + 1);
-console.log(Boolean(1));
 console.log(Boolean(0));
-console.log(Boolean(231));
-console.log(Boolean("qwerty"));
 console.log(Boolean(""));
-console.log(true + 2);
-
-const a = 2 + "10"
-console.log(a - 10);
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(Number("sx")));     // number"sx" = NaN = 0 = false
