f28c30599f5abb9fea5a65c0762cb0385b5a2e01
Said <udiejfkskfid@gmail.com>
2024-04-23 20:28:59 +0500

    Map

---
+++ JavaScript/app.js
@@ -0,0 +1,34 @@
+// const score = [5, 10, 0, 15];
+
+// const totalScore = score.reduce((acc, curr) => acc + curr, 0);
+
+// console.log(`Общее количество очков: ${totalScore}`);
+
+// // for (const [i, el] of score.entries()) {
+// //     console.log(`Round ${i + 1}: ${el} Points`);
+// // }
+
+// score.forEach(function (scoreEl, i, arr){
+//     console.log(`Round ${i + 1}: ${scoreEl} Points`);
+// })
+
+// // (5,0) => { ... }
+// // (10,1) => { ... }
+
+const transactionsInUsd = [10, -7, 50, -10, 100];
+const transactionInRub = [];
+for (const transaction of transactionsInUsd) {
+    transactionInRub.push(transaction * 60);
+}
+
+
+console.log('Old_transactionsInUsd:', transactionsInUsd);
+console.log('Old_transactionInRub:', transactionInRub);
+
+const transactionsInRub2 = transactionsInUsd.map((transaction, i) => {
+    return transaction * 60;
+})
+
+
+console.log('transactionsInUsd :', transactionsInUsd);
+console.log('transactionInRub :', transactionsInRub2);
